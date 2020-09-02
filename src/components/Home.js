a089e5a26c890507b0d550a44e3f669b8541f52f
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-09-02 23:32:30 +0530

    Slide in button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,10 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <motion.div
-      className="home container"
-      animate={{ opacity: 0.2, marginTop: 200, rotateZ: 45 }}
-    >
-      <motion.h2 animate={{ scale: 1.5, color: "#ff2994", x: 100, y: -50 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div className="home container" animate={{}}>
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <motion.button whileHover={{ scale: 1.5 }}>
-          Create Your Pizza
-        </motion.button>
+        <motion.button whileHover={{}}>Create Your Pizza</motion.button>
       </Link>
     </motion.div>
   );
